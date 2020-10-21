27d3ef8dcc5675445c1c631f1809265429ca98fb
Cheng <liuruiqi1107@gmail.com>
2020-10-21 22:56:56 +0200

    policy load rewrite

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import gym
-from PPO import PPO, Memory
+from PPOv2 import PPO, Memory
 from PIL import Image
 import torch
 
@@ -12,7 +12,7 @@ def test():
     action_dim = 4
     render = False
     max_timesteps = 500
-    n_latent_var = 64           # number of variables in hidden layer
+    hidden_dim = 64           # number of variables in hidden layer
     lr = 0.0007
     betas = (0.9, 0.999)
     gamma = 0.99                # discount factor
@@ -29,15 +29,15 @@ def test():
     directory = "./preTrained/"
     
     memory = Memory()
-    ppo = PPO(state_dim, action_dim, n_latent_var, lr, betas, gamma, K_epochs, eps_clip)
+    ppo = PPO(state_dim, action_dim, hidden_dim, lr, gamma, K_epochs, eps_clip)
     
-    ppo.policy_old.load_state_dict(torch.load(directory+filename), strict=False)
+    ppo.policy.load_state_dict(torch.load(directory+filename), strict=True)
     
     for ep in range(1, n_episodes+1):
         ep_reward = 0
         state = env.reset()
         for t in range(max_timesteps):
-            action = ppo.policy_old.act(state, memory)
+            action = ppo.policy.act(state, memory)
             state, reward, done, _ = env.step(action)
             ep_reward += reward
             if render:
